2f6b398d8f5510eb1be287f32115df42c50a2d1a
PRANAVBHALE <bhalepranav25@gmail.com>
2018-10-09 23:47:04 +0530

    completed /process API

--- routes/index.js
+++ routes/index.js
@@ -2,13 +2,30 @@ var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get("/", (req, res) => {
+router.get("/process/*", (req, res) => {
   //console.log(req._startTime);
+  console.log('in ',req);
+  
   var time = req._startTime
+  var methodType = req.method
+  var headers = req.headers
+  var path = req.originalUrl
+  var query = req.query
+  var body = req.body
+  var responseTime = Date.now() - time
+
+
 
  // res.json({ op: req });
   res.json({
-    time : time
+    time : time,
+    methodType:methodType,
+    headers:headers,
+    query:query,
+    path:path,
+    body:body,
+    responseTime:responseTime + "secs"
+
   })
 });
 
